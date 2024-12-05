06f196105f930109c0da4b36092c0e4ac5b2b980
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-05 20:53:37 -0300

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -17,10 +17,10 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = fixCaseSensitive(prompt("Choose your option between Rock, Paper or Scissors:"));
-  if (humanChoice != "Rock" && humanChoice != "Paper" && humanChoice != "Scissors") {
+  if (humanChoice != "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors") {
     do {
-      humanChoice = fixCaseSensitive(prompt("Invalid choice, please choose a valid option between Rock, Paper or Scissors:"));
-    } while (humanChoice != "Rock" && humanChoice != "Paper" && humanChoice != "Scissors");
+      humanChoice = fixCaseSensitive(prompt("*Invalid choice* Please choose a valid option between Rock, Paper or Scissors:"));
+    } while (humanChoice != "Rock" && humanChoice !== "Paper" && humanChoice !== "Scissors");
   }
   console.log(`You chose: ${humanChoice}`);
   return humanChoice;
@@ -30,4 +30,29 @@ function fixCaseSensitive (stringToFix) {
   return String(stringToFix.charAt(0).toUpperCase()) + String(stringToFix.slice(1).toLowerCase());
 }
 
-getHumanChoice();
+function playRound (humanSelection, computerSelection) {
+  if (humanSelection === computerSelection) {
+    console.log(`It's a draw! Both you can the computer chose ${humanSelection}.`);
+  } else if (humanSelection === "Rock" && computerSelection === "Paper"){
+    console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);
+    computerScore++;
+  } else if (humanSelection === "Rock" && computerSelection === "Scissors"){
+    console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
+    humanScore++;
+  } else if (humanSelection === "Paper" && computerSelection === "Rock"){
+    console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
+    humanScore++;
+  } else if (humanSelection === "Paper" && computerSelection === "Scissors"){
+    console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);
+    computerScore++;
+  } else if (humanSelection === "Scissors" && computerSelection === "Rock"){
+    console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);
+    computerScore++;
+  } else if (humanSelection = "Scissors" && computerSelection == "Paper"){
+    console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
+    humanScore++;
+  } else {
+    console.log("Uh Oh.. Something went wrong.")
+  }
+  return;
+}
