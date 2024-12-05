8ea0fab0add50dc8f60955c2bfc4fb74f5f9f9d4
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-05 04:37:02 -0300

    Tweak getComputerChoice for more fairness
    The Math.floor() would cause any result below 0.01 to turn into a zero,
    making if slightly less fair for the Rock option. It's better to just
    use Math.random() alone.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,14 @@
-
 function getComputerChoice() {
   let computerChoice = "";
-  let randomNumber = Math.floor((Math.random() * 100));
-  if (randomNumber > 0 && randomNumber <= 33) {
+  let randomNumber = Math.random();
+  if (randomNumber > 0 && randomNumber <= 0.33) {
     computerChoice = "Rock";
-  } else if (randomNumber > 33 && randomNumber <= 66) {
+  } else if (randomNumber > 0.33 && randomNumber <= 0.66) {
     computerChoice = "Paper";
   } else {
     computerChoice = "Scissors";
   }
+  console.log(randomNumber);
+  console.log(computerChoice);
   return computerChoice;
 }
-
-console.log(getComputerChoice());
