5c11711d412eeed9ae6f70c5d6a8689acea88c68
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2024-12-05 04:21:20 -0300

    Add getComputerChoice function
    This function returns an equally random option for each of the three
    possible choices that the computer can make for the game.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+  let computerChoice = "";
+  let randomNumber = Math.floor((Math.random() * 100));
+  if (randomNumber > 0 && randomNumber <= 33) {
+    computerChoice = "Rock";
+  } else if (randomNumber > 33 && randomNumber <= 66) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
+  return computerChoice;
+}
+
+console.log(getComputerChoice());
