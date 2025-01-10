938f9b1381d8be7e2a3a1ceb066f4e57104b6fa1
spiderzoast <161398574+spiderzoast@users.noreply.github.com>
2025-01-10 20:56:32 -0300

    Rework logic for the entire game and add UI

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+let userScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
+
+const gameContainer = document.querySelector("#game-container");
+
+const gameOptions = document.querySelector("#game-options");
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    playRound(event.target.textContent, getComputerChoice());
+  });
+});
+
 function getComputerChoice() {
   let computerChoice = "";
   let randomNumber = Math.random();
@@ -12,34 +28,64 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function playRound (humanSelection, computerSelection) {
+function playRound(humanSelection, computerSelection) {
+  roundCounter += 1;
   if (humanSelection === computerSelection) {
-        console.log(`It's a draw! Both you and The Computer chose ${humanSelection}.`);
-        return 0;
+      roundTie(humanSelection);
   } else if (
       (humanSelection === "Rock" && computerSelection === "Scissors") ||
       (humanSelection === "Paper" && computerSelection === "Rock") ||
       (humanSelection === "Scissors" && computerSelection === "Paper")
-    ) {
-        console.log(`You win! ${humanSelection} beats ${computerSelection}.`);
-        return 1;
+  ) {
+      userWins(humanSelection, computerSelection);
   } else if (
       (humanSelection === "Rock" && computerSelection === "Paper") ||
       (humanSelection === "Paper" && computerSelection === "Scissors") ||
       (humanSelection === "Scissors" && computerSelection === "Rock")
-    ) {
-        console.log(`You lose! ${computerSelection} beats ${humanSelection}.`);
-        return 2;
-  } else {
-        console.log("Uh Oh.. Something went wrong.")
-        return 0;
+  ) {
+      computerWins(humanSelection, computerSelection);
   }
+  if (userScore === 5 || computerScore === 5) {
+    announceWinner();
+  }
+  return;
 }
 
-const buttons = document.querySelectorAll(".btn");
+function roundTie(humanSelection) {
+  const thisRound = document.createElement("p");
+  thisRound.textContent = `Round ${roundCounter}: It's a tie! Both you and the computer chose ${humanSelection}.`;
+  gameContainer.appendChild(thisRound);
+  return;
+}
 
-buttons.forEach((button) => {
-  button.addEventListener("click",() => {
-    playRound(getComputerChoice(), button.textContent);
-  });
-});
+function userWins(humanSelection, computerSelection) {
+  userScore += 1;
+  const thisRound = document.createElement("p");
+  thisRound.textContent = `Round ${roundCounter}: You win! ${humanSelection} beats ${computerSelection}. — You: ${userScore} The Computer: ${computerScore}`;
+  gameContainer.appendChild(thisRound);
+  return;
+}
+
+function computerWins(humanSelection, computerSelection) {
+  computerScore += 1;
+  const thisRound = document.createElement("p");
+  thisRound.textContent = `Round ${roundCounter}: You lose! ${computerSelection} beats ${humanSelection}. — You: ${userScore} The Computer: ${computerScore}`;
+  gameContainer.appendChild(thisRound);
+  return;
+}
+
+function announceWinner() {
+  gameOptions.remove();
+  console.log(userScore);
+  console.log(computerScore);
+  if (userScore === 5) {
+    const gameWinner = document.createElement("h4");
+    gameWinner.textContent = "You win the game! You reached 5 wins first. — (Reload the page to play again.)";
+    gameContainer.appendChild(gameWinner);
+  } else {
+    const gameWinner = document.createElement("h4");
+    gameWinner.textContent = "You lose the game! The computer reached 5 wins first... good luck next time! — (Reload the page to play again.)";
+    gameContainer.appendChild(gameWinner);
+  }
+  return;
+}
